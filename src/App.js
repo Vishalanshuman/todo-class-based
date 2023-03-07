499f17b46a1e2294525d7c6f86ba3ec849e4253f
Vishalanshuman <vishalanshuman@gmail.com>
2023-03-07 14:08:03 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import { Navbar } from "./components/Navbar";
+import { ToDoRows } from "./components/ToDoRows";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      userName:"Vshal",
+      todoItems: [
+        {action:"Buy Milk", done:true},
+        {action:"Dentist at 5pm", done:false},
+        {action:"Go To Gym", done:false},
+      ],
+      newTodo:"",
+    };
+  }
+  updateValue = (event) =>{
+    this.setState({newTodo: event.target.value})
+  }
+
+  newTodo = () => {
+    this.setState({
+      todoItems:[
+        ...this.state.todoItems,
+        {action:this.state.newTodo, done:false}
+      ]
+    })
+  }
+  toggleDone = (todo) =>
+      this.setState({
+        todoItems: this.state.todoItems.map((item) => 
+          item.action === todo.action ? {...item,done: !item.done} : item
+        ),
+      });
+  todoRows = () => this.state.todoItems.map((item)=>
+  <ToDoRows key={item.action} item={item} callback={this.toggleDone} />);
+
+  // changeStateData=()=>{
+  //   this.setState({
+  //     userName: this.state.userName === "Vshal" ? "Mandan" : "Vshal",
+  //   });
+  // }
+  render=()=>(
+    <div className="container">
+      <div className="row">
+        <Navbar name={this.state.userName}/>
+
+        <div className="col-12">
+          <input className="form-control" value={this.state.newTodo}
+          onChange={this.updateValue} />
+
+          <button onClick={this.newTodo} className="btn btn-primary"> Add</button>
+        </div>
+        <div className="col-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Task</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoRows()}</tbody>
+
+          </table>
+
+        </div>
+      </div>
+      {/* <button
+       className="btn btn-secondary m2" 
+       onClick={this.changeStateData}
+       >
+        change
+      </button> */}
     </div>
-  );
-}
 
-export default App;
+  )
+}
